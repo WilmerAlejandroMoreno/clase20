c358a202015494f67b60f198c9bbdcf3282a143e
WilmerAlejandroMoreno <Wilmeramv2810@gmail.com>
2021-10-27 17:59:00 -0500

    clase20

---
+++ dom.js
@@ -0,0 +1,22 @@
+//dom
+//document objet model
+//modelo de abjeto del documento    
+/*console.log( document.getElementById("tituloNombre"));
+console.log( document.getElementById("nombre"));
+console.log( document.getElementById("apellido"));
+console.log( document.getElementById("correo"));
+console.log( document.getElementById("telefono"));
+console.log( document.getElementById("tituloApellido"));
+console.log( document.getElementById("titulocorreo"));
+console.log( document.getElementById("titulotelefono"));*/
+//document.getelemetbyname
+/*console.log ( document.getElementsByName("nombre"));
+console.log ( document.getElementsByName("apellido"));
+console.log ( document.getElementsByName("correo"));
+console.log ( document.getElementsByName("telefono"));*/
+//document.getElementsByClassName("clase")
+console.log(document.getElementsByTagName("form"))
+console.log(document.getElementsByTagName("label"))
+console.log(document.getElementsByTagName("input"))
+console.log(document.getElementsByTagName("br"))
+console.log(document.getElementsByTagName("script"))
